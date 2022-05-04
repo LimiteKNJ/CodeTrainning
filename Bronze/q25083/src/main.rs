e0d913bacee8992752c146c297d3fb8869ccafab
LimiteKNJ <kuoc123@naver.com>
2022-05-04 17:24:37 +0900

    solved q25083 by Rust..?

---
+++ Bronze/q25083/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("         ,r'\"7");
+    println!("r`-_   ,\'  ,/");
+    println!("\\. \". L_r\\'");
+    println!("  `~\\/");
+    println!("     |");
+    println!("     |");
+}
+/* 
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+
+*/
